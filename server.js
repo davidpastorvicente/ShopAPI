68ce2d1f68b38822d580a6ed2c08c92731b357d7
David <davidpastorvicente@gmail.com>
2020-08-17 12:33:39 +0200

    Add Wishlists functions

--- server.js
+++ server.js
@@ -11,16 +11,42 @@ app.use(bp.urlencoded({extended: false}));
 
 app.post('/product', function(req, res) {
   var product = new Product(req.body);
-  product.save(function(err, save) {
+  product.save(function(err, prod) {
     if(err) res.status(500).send({error: 'Could not save'});
-    else res.status(200).send(save);
+    else res.status(200).send(prod);
   })
 });
 
 app.get('/product', function(req, res) {
-  Product.find({}, function(err, save) {
+  Product.find({}, function(err, prod) {
     if(err) res.status(500).send({error: 'Could not display'});
-    else res.status(200).send(save);
+    else res.status(200).send(prod);
+  });
+});
+
+app.post('/wishlist', function(req, res) {
+  var wishlist = new Wishlist();
+  wishlist.title = req.body.title;
+  wishlist.save(function(err, wish) {
+    if(err) res.status(500).send({error: 'Could not save'});
+    else res.status(200).send(wish);
+  });
+});
+
+app.get('/wishlist', function(req, res) {
+  Wishlist.find({}, function(err, wish) {
+    if(err) res.status(500).send({error: 'Could not display'});
+    else res.status(200).send(wish);
+  });
+});
+
+app.put('/wishlist/add', function(req, res) {
+  Product.findOne({_id: req.body.productId}, function(err, prod) {
+    if(err) res.status(500).send({error: 'Could not find the product'});
+    else Wishlist.update({_id: req.body.wishlistId}, {$addToSet: {products: prod._id}}, function(err, wish) {
+      if(err) res.status(500).send({error: 'Could not find the wishlist'});
+      else res.status(200).send(wish);
+    });
   });
 });
 
